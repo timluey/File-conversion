c498f2efc001155cf94d1edeaa576df019a56d97
timluey <timothyluey@gmail.com>
2022-02-23 14:48:53 +1300

    package

---
+++ Package/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = ''
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="FormusLabs_project", 
+        version=VERSION,
+        author="Timothy Luey",
+        author_email="timothyluey@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
